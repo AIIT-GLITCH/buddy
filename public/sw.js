f45e9123c7863b301f6358195fda2e1725f74066
Rhet Dillard Wike <reliablerestaurantrepair@gmail.com>
2026-05-20 22:00:31 -0500

    Bump Buddy nav cache version

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var VERSION = '2026-05-20.6';
+var VERSION = '2026-05-20.7';
 var STATIC_CACHE = 'aiit-static-' + VERSION;
 var PAGE_CACHE = 'aiit-pages-' + VERSION;
 var RUNTIME_CACHE = 'aiit-runtime-' + VERSION;
