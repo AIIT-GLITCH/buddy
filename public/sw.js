646362f752b6dc8468b596e2c03403ca647ae562
Rhet Dillard Wike <reliablerestaurantrepair@gmail.com>
2026-05-20 21:16:18 -0500

    Add Buddy thread share CTA

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var VERSION = '2026-05-20.2';
+var VERSION = '2026-05-20.3';
 var STATIC_CACHE = 'aiit-static-' + VERSION;
 var PAGE_CACHE = 'aiit-pages-' + VERSION;
 var RUNTIME_CACHE = 'aiit-runtime-' + VERSION;
