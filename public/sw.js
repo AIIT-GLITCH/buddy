a80e162cce5a30813da8acb44d6ca9252a755856
AIIT-GLITCH <wikerdw1@gmail.com>
2026-07-17 17:51:57 -0500

    Enable browser-local companion chats (2/6)

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var VERSION = '2026-07-10.08';
+var VERSION = '2026-07-17.01';
 var STATIC_CACHE = 'aiit-static-' + VERSION;
 var PAGE_CACHE = 'aiit-pages-' + VERSION;
 var RUNTIME_CACHE = 'aiit-runtime-' + VERSION;
