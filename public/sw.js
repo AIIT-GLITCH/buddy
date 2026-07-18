2426a64f73b17f7eec49903b007f5ddb9efb644e
AIIT-GLITCH <wikerdw1@gmail.com>
2026-07-18 02:46:39 -0500

    SMS compliance: update public/sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var VERSION = '2026-07-17.01';
+var VERSION = '2026-07-18.01';
 var STATIC_CACHE = 'aiit-static-' + VERSION;
 var PAGE_CACHE = 'aiit-pages-' + VERSION;
 var RUNTIME_CACHE = 'aiit-runtime-' + VERSION;
